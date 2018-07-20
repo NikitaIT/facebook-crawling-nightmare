ceb4854f340d3224c184d61fc2f13ae5d03ca340
NikitaIT <nikita2008-101@yandex.ru>
2018-07-20 22:16:08 +0300

    #1 add tests

---
+++ src/tests/test.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Test function', () => {
+    it('should return Test works', () => {
+      expect('Test works').to.equal('Test works');
+    });
+});
